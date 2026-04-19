7c08cf303e2ce7ef623d286f9654f9b1d974f036
092wb <you@example.com>
2026-04-19 22:56:47 +0800

    first commit

---
+++ src/client/components/FilesPageMain.vue
@@ -0,0 +1,63 @@
+<script setup lang="ts">
+import PageHeader from "./PageHeader.vue";
+import PagBreadcrumb from "./PagBreadcrumb.vue";
+import MainBox from "./MainBox.vue";
+import FLink from "./FButtonLink.vue";
+import {usePageFrontmatter} from "vuepress/client";
+import Notification from "../js/notification/Notification.vue";
+import { FilePageFrontmatter, FolderPageFrontmatter } from "../../type";
+import { computed } from "vue";
+import FContent from "./FContent.vue";
+
+const frontmatter = usePageFrontmatter<FolderPageFrontmatter|FilePageFrontmatter>();
+const content = computed(()=>frontmatter.value.flistData?.content);
+
+</script>
+
+<template>
+  <div class="page-main">
+    <div class="page-layouts">
+      <PageHeader></PageHeader>
+      <PagBreadcrumb></PagBreadcrumb>
+      <MainBox v-if="$slots.default">
+        <slot></slot>
+      </MainBox>
+      <MainBox v-else>
+        <div class="empty">没有找到如何文件,请在mounts文件夹中添加挂载!</div>
+      </MainBox>
+      <FContent v-if="content" :content="content" style="padding: 1rem"></FContent>
+    </div>
+    <div class="footer">
+      <FLink to="https://github.com/092wb/092wb">092Team By 往风</FLink>
+    </div>
+<!--    消息弹出框-->
+    <Notification></Notification>
+  </div>
+</template>
+
+<style scoped>
+.empty{
+  text-align: center;
+}
+.page-main{
+  background-color: var(--page-bgc);
+  padding: 0 2%;
+  min-height: 100vh;
+}
+.page-layouts {
+  margin: 0 auto;
+  max-width: 980px;
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
+  min-height: calc(100vh - 6rem);
+}
+.footer{
+  height: 6rem;
+  display: flex;
+  justify-content: center;
+  gap: 0.5rem;
+  align-items: center;
+}
+
+</style>
---
+++ src/node/index.ts
@@ -0,0 +1,43 @@
+import {App, Theme,} from "vuepress";
+import { getDirname, path } from 'vuepress/utils'
+import {createFileTreePages} from "./base/pages.js";
+import {allAnalysis} from "./base/AllAnalysis.js";
+import {
+    callExtendsBundlerOptions,
+    callOnGenerated,
+    callOnInitialized,
+    callOnWatched,
+    Closable
+} from "./base/eventManager.js";
+import {nprogressPlugin} from "@vuepress/plugin-nprogress";
+import { loadAnalysisConfigFromDir } from "./config/loadAnalysisConfigFromDir.js";
+
+const __dirname = getDirname(import.meta.url)
+
+export function FileList():Theme{
+    return ()=>{
+        return {
+            name:"FList",
+            clientConfigFile: path.join(__dirname, "../client/index.ts"),
+            plugins: [
+                nprogressPlugin()
+            ],
+            onInitialized:async (app)=>{
+                await callOnInitialized(app);
+                const analysisConfig = loadAnalysisConfigFromDir("mounts");
+                const fileTree = await allAnalysis(analysisConfig);
+                const pageList = await Promise.all(createFileTreePages(app,fileTree));
+                app.pages.push(...pageList);
+            },
+            onGenerated:async (app)=>{
+                await callOnGenerated(app);
+            },
+            onWatched:async (app: App, watchers: Closable[], restart: () => Promise<void>)=>{
+                await callOnWatched(app, watchers, restart);
+            },
+            extendsBundlerOptions: async (options,app)=>{
+                await callExtendsBundlerOptions(options,app);
+            }
+        }
+    }
+}
